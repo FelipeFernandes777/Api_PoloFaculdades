e3f1e300f8e1da6097f99749e018244f027a6708
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-08-29 18:47:22 -0300

    feat: create lead model

--- src/main/java/br/com/api/polofaculdades/models/lead/LeadModel.java
+++ src/main/java/br/com/api/polofaculdades/models/lead/LeadModel.java
@@ -1,4 +1,32 @@
 package br.com.api.polofaculdades.models.lead;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Table(name = "leads")
+@Entity(name = "lead")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
 public class LeadModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "nome", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "telefone", nullable = false, length = 11)
+    private String phone;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 }
