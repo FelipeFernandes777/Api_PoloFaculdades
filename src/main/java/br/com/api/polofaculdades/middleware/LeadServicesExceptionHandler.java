830e363a1ae3adfb6a0f42107afcf88e9f075d7b
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-09-02 20:53:37 -0300

    feat: create LeadServicesExceptionHandler

--- src/main/java/br/com/api/polofaculdades/middleware/LeadServicesExceptionHandler.java
+++ src/main/java/br/com/api/polofaculdades/middleware/LeadServicesExceptionHandler.java
@@ -2,21 +2,28 @@ package br.com.api.polofaculdades.middleware;
 
 import br.com.api.polofaculdades.exception.enterprise.EnterpriseNotFoundException;
 import br.com.api.polofaculdades.exception.enterprise.InvalidEnterpriseDataException;
+import br.com.api.polofaculdades.exception.lead.InvalidLeadDataException;
+import br.com.api.polofaculdades.exception.lead.NotFoundLeadException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class EnterpriseServicesExceptionHandler {
+public class LeadServicesExceptionHandler {
 
-    @ExceptionHandler(InvalidEnterpriseDataException.class)
-    public ResponseEntity<String> invalidEnterpriseDataExceptionHandler(InvalidEnterpriseDataException ex) {
+    @ExceptionHandler(InvalidLeadDataException.class)
+    public ResponseEntity<String> invalidLeadDataException(InvalidLeadDataException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
-    @ExceptionHandler(EnterpriseNotFoundException.class)
-    public ResponseEntity<String> enterpriseNotFoundExceptionHandler(EnterpriseNotFoundException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+    @ExceptionHandler(NotFoundLeadException.class)
+    public ResponseEntity<String> notFoundLeadException(NotFoundLeadException ex) {
+        return ResponseEntity.status(404).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(500).body("Erro interno: " + ex.getMessage());
     }
 
 }
