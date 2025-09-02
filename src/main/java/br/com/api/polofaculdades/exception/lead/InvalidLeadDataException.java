0b0b0dc31d3e45f3f7402bd55878ab6e0ff5e495
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-09-02 20:50:11 -0300

    feat: Create exceptions for lead services

--- src/main/java/br/com/api/polofaculdades/exception/lead/InvalidLeadDataException.java
+++ src/main/java/br/com/api/polofaculdades/exception/lead/InvalidLeadDataException.java
@@ -1,7 +1,7 @@
 package br.com.api.polofaculdades.exception.lead;
 
 public class InvalidLeadDataException extends RuntimeException {
-  public InvalidLeadDataException(String message) {
-    super(message);
-  }
+    public InvalidLeadDataException(String message) {
+        super(message);
+    }
 }
--- src/main/java/br/com/api/polofaculdades/exception/lead/NotFoundLeadException.java
+++ src/main/java/br/com/api/polofaculdades/exception/lead/NotFoundLeadException.java
@@ -1,7 +1,7 @@
 package br.com.api.polofaculdades.exception.lead;
 
 public class NotFoundLeadException extends RuntimeException {
-  public NotFoundLeadException(String message) {
-    super(message);
-  }
+    public NotFoundLeadException(String message) {
+        super(message);
+    }
 }
